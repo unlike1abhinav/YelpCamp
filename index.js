f8a11080692f5366192b0cc6fe6d413439653825
unlike1abhinav <dhimanabhinav8295@gmail.com>
2024-10-11 05:10:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (request, response) => {
+    response.send('Hello from Yelp Camp')
+})
+
+app.listen(8080, () => {
+    console.log('Listen On 8080')
+})
